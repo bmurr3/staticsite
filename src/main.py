3f69fffc409186a02e5b0375406b59ecdd0f72a5
bmurr3 <wdmurrayiii98@gmail.com>
2024-12-25 16:14:55 -0500

    Adding TextNode

---
+++ src/main.py
@@ -0,0 +1,5 @@
+from textnode import TextNode, TextType
+
+
+test_node = TextNode('This is a test', TextType.BOLD, 'localhost:8888')
+print(test_node)
---
+++ src/textnode.py
@@ -0,0 +1,35 @@
+from enum import StrEnum
+
+
+class TextType(StrEnum):
+    NORMAL = "normal"
+    BOLD = "bold"
+    ITALIC = "italic"
+    CODE = "code"
+    LINK = "link"
+    IMAGE = "image"
+
+
+class TextNode():
+    def __init__(
+        self,
+        text: str,
+        text_type: TextType,
+        url: str | None = None
+    ) -> None:
+        self.text: str = text
+        self.text_type: TextType = text_type
+        self.url: str | None = url
+
+    def __eq__(self, value: "TextNode") -> bool:
+        return (
+            self.text == value.text,
+            self.text_type == value.text_type,
+            self.url == value.url
+        )
+    
+    def __repr__(self) -> str:
+        text = self.text
+        text_type = self.text_type.value
+        url = self.url
+        return f'{type(self).__name__}({text=}, {text_type=}, {url=}, {id(self)=})'
